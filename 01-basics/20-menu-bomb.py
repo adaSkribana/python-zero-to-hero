9b8e917b47eb6b2afb0ea9d15336e1ff939721d8
adaSkribana <136006242+adaSkribana@users.noreply.github.com>
2024-02-23 17:56:00 -0300

    Update 20-menu-bomb.py
    new version

--- 01-basics/20-menu-bomb.py
+++ 01-basics/20-menu-bomb.py
@@ -2,14 +2,25 @@ import time
 # Agregar un menú de 2 opciones para ver la bomba de tiempo
 # 1 ver bomba de tiempo 2 para salir
 
-user_option = 0
-while user_option != 2:
-  user_option = int(input("Ingresa 1 para ver la bomba, 2 para salir"))
-  
-  if user_option == 1:
+def boom():
     num = 10
     while (num >= 0):
       print(num)
       time.sleep(1)
       num = num - 1
     print("Boom")
+
+while True:
+  try:
+    print("escoje entre las siguientes opciones 1 - ver bomba / 2- salir")
+    user=int(input("Ingrese su eleccion: "))
+    if user <1 or user > 2:
+      print("Ingrese una opcion valida 1/2")
+    elif user == 1:
+      boom()
+    else:
+      print("Hasta luego")
+      break
+  except ValueError:
+    print("Ingrese una opcion valida.")
+    continue
